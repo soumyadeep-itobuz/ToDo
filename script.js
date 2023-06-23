60912b4d111e867cfbd771a9174672d63b323702
soumyadeep-itobuz <soumyadeep@itobuz.com>
2023-06-23 18:40:49 +0530

    wip

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let arrDo = [];
+const arrDo = [];
 function add() {
-    document.getElementById('listContainer').innerHTML="";
-    arrDo.unshift(document.getElementById("text").value);
-    // document.getElementById('list').innerHTML = arrDo.join("<button onclick=remove()>Delete</button> <br>");
-    for(let i=0;i<arrDo.length;i++) {
-        document.getElementById('listContainer').innerHTML += `<div id="list" class="toDo-list d-flex justify-content-center mt-5 gap-3">
-    <input class="taskList p-3" type="text" id="toDo">
-    <button
-      onclick="complete()"
-      class="align-self-center bg-black text-white p-1 completeButton"
-    >
-      &#10003;
-    </button>
-    <button
-      onclick="remove()"
-      class="align-self-center bg-black text-white p-1 removeButton"
-    >
-      <i class="fa-solid fa-trash-can"></i>
-    </button>
-  </div>`;
-    }
-   }
-function remove() {
-    arrDo.splice(index(),1);
+  arrDo.push(document.getElementById("text").value);
+  showList();
+  console.log(arrDo);  
 }
+
+function showList(){
+  document.getElementById('listContainer').innerHTML = ""; //empty the array
+  console.log(arrDo.length);
+  for (let i = 0; i < arrDo.length; i++) {
+    document.getElementById('listContainer').innerHTML += ` <div class="d-flex border w-75 align-self-center justify-content-around gap-0 id="list"">
+        <input class="taskList p-3 border-0" type="text" id="toDo" value="${arrDo[i]}"/>
+        <button
+          onclick="complete('${i}')"
+          class="align-self-center bg-black text-white p-1 completeButton">&#10003;
+        </button>
+        <button
+          onclick="remove('${i}')"
+          class="align-self-center bg-black text-white p-1" id = "removeButton">
+          <i class="fa-solid fa-trash-can"></i>
+        </button>
+    </div> `
+  }
+  document.querySelector("#text").value = "";
+}
+
+function remove(i) {
+  arrDo.splice(i, 1);
+  showList()
+  console.log(arrDo);
+}
+
